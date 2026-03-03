8105661c3fa7ad011f2d6313dca72a82f9749e95
Nitheesh14125 <s.nitheesh14125@gmail.com>
2026-03-04 04:00:41 +0530

    About url which return aboutme details

--- main.py
+++ main.py
@@ -6,4 +6,18 @@ app = FastAPI()
 def index():
     return 'Hey hi'
 
+@app.get('/') #returing a json api 
+def json():
+    return {"data": {"name": "Nitheesh"}}
 
+
+@app.get('/about')
+def about():
+    return {
+        "data":{
+            "name":"S.Nitheesh",
+            "age":19,
+            "Gender":"Male",
+            "Profession":"Student"
+        }
+    }
